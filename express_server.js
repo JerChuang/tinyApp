6a8a3b31f1c294ec51e56d408f74da0c92db80d6
Jerry <chih.chia.chuang@gmail.com>
2019-05-08 01:50:20 +0000

    added function to redirect /u/:shortURL to long url

--- express_server.js
+++ express_server.js
@@ -51,6 +51,11 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+    const longURL = urlDatabase[req.params.shortURL];
+    res.redirect(longURL);
+  });
+
 app.get("/hello", (req, res) => {
   let templateVars = { greeting: 'Hello World!' };
   res.render("hello_world", templateVars);
