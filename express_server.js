5f860f5a0c44a8ae7227732c0d5969c74ad7bc60
Jerry <chih.chia.chuang@gmail.com>
2019-05-10 03:24:47 +0000

    Added readme file and screenshots

--- express_server.js
+++ express_server.js
@@ -1,7 +1,7 @@
 // Required Modules:
 const express = require('express');
-const morgan = require('morgan'); // console logs get/post request to help with development
-const cookieParser = require('cookie-parser'); 
+const morgan = require('morgan'); // module that console logs get/post request to help with development
+const cookieSession = require('cookie-session');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 const app = express();
@@ -10,29 +10,33 @@ const PORT = 8080; // default port 8080
 app.set('view engine', 'ejs');
 
 app.use(morgan('dev'));
-app.use(cookieParser());
+app.use(cookieSession({
+  name: 'session',
+  keys: ['hippopotamus'],
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
 app.use(bodyParser.urlencoded({extended: true}));
 
 // Gloal Objects:
 const urlDatabase = {
-  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
-  i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" },
-  iaDFej: { longURL: "http://lighthouse.ca", userID: "kRYfIf"}
+  b6UTxQ: { longURL: 'https://www.tsn.ca', userID: 'aJ48lW' },
+  i3BoGr: { longURL: 'https://www.google.ca', userID: 'aJ48lW' },
+  iaDFej: { longURL: 'http://lighthouse.ca', userID: 'kRYfIf'}
 };
 
 const users = { 
-  "aJ48lW": {
-    id: "aJ48lW", 
-    email: "test@test.com", 
-    password: "$2b$12$j49neYspr/OIvLHiv6Y1zOSqnzltqdTo9ZokeLiTdVGebcgrsprWa"
+  'aJ48lW': {
+    id: 'aJ48lW', 
+    email: 'test@test.com', 
+    password: '$2b$12$j49neYspr/OIvLHiv6Y1zOSqnzltqdTo9ZokeLiTdVGebcgrsprWa'
   },
-  "kRYfIf": {
-    id: "kRYfIf", 
-    email: "user2@example.com", 
-    password: "$2b$12$KdvxpFZrhGLp4Af1ghCz4uUkkWHssRM0wuvbXEUfY3bBdICOPMFcy"
+  'kRYfIf': {
+    id: 'kRYfIf', 
+    email: 'user2@example.com', 
+    password: '$2b$12$KdvxpFZrhGLp4Af1ghCz4uUkkWHssRM0wuvbXEUfY3bBdICOPMFcy'
   }
 }
-
 
 // Global functions:
 function generateRandomString() {
@@ -44,7 +48,7 @@ function generateRandomString() {
   return output;
 }
 
-function emailLookUp(email){
+function emailLookUp(email){            //look up all e-mails in users object and return the matching user or return false if no match
   for (let person in users){
     if (users[person].email === email){
       return users[person];
@@ -53,7 +57,7 @@ function emailLookUp(email){
   return false;
 }
 
-function urlsForUser(id){
+function urlsForUser(id){           //search for links that belong to the user, return new object with all links created by user
   let output = {};
   for (let links in urlDatabase){
     if (urlDatabase[links].userID === id){
@@ -63,10 +67,9 @@ function urlsForUser(id){
   return output;
 }
 
-
 // Server endpoints:
 app.get('/', (req, res) => {
-  if (req.cookies.user_id){
+  if (req.session.user_id){
     res.redirect('/urls');
   } else {
     res.redirect('/login')
@@ -75,16 +78,16 @@ app.get('/', (req, res) => {
 
 app.get('/urls', (req, res) => {
   const templateVars = {
-    urls: urlsForUser(req.cookies.user_id),
-    user: users[req.cookies.user_id]
+    urls: urlsForUser(req.session.user_id),
+    user: users[req.session.user_id]
   };
   res.render('urls_index', templateVars);
 });
 
 app.get('/urls/new', (req, res) => {
-  if(req.cookies.user_id){
+  if(req.session.user_id){
     const templateVars = {
-      user: users[req.cookies.user_id]
+      user: users[req.session.user_id]
     };
     res.render('urls_new', templateVars);
   } else {
@@ -93,8 +96,8 @@ app.get('/urls/new', (req, res) => {
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-  const userlink = urlsForUser(req.cookies.user_id)
-  if (!urlDatabase[req.params.shortURL] || !userlink[req.params.shortURL]){  //Make sure the shortURL link exists in the first place so website doesn't crash
+  const userlink = urlsForUser(req.session.user_id)
+  if (!urlDatabase[req.params.shortURL] || !userlink[req.params.shortURL]){  //check if link exists or belong to the user
     res.status('400');
     res.send('This url does not exist or not available for you to edit')
   }
@@ -102,7 +105,7 @@ app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { 
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL].longURL, 
-    user: users[req.cookies.user_id]
+    user: users[req.session.user_id]
   };
   res.render('urls_show', templateVars);
 });
@@ -112,18 +115,22 @@ app.get('/u/:shortURL', (req, res) => {
   res.redirect(longURL);
 });
 
-
 app.post('/urls', (req, res) => {
-  const short = generateRandomString();
-  urlDatabase[short] = {
-    longURL: req.body.longURL,
-    userID: req.cookies.user_id
-  }; // updating urlDatabase with objects of shortURL: longURL/userID 
-  res.redirect('/urls/'+short);
+  if (req.session.user_id){
+    const short = generateRandomString();
+    urlDatabase[short] = {              // updating urlDatabase with objects of shortURL: longURL/userID 
+      longURL: req.body.longURL,
+      userID: req.session.user_id
+    }; 
+    res.redirect('/urls/'+short);
+  } else {
+    res.status('400');
+    res.send('Please login')
+  }
 });
 
 app.post('/urls/:shortURL', (req, res) => {
-  const userlink = urlsForUser(req.cookies.user_id);
+  const userlink = urlsForUser(req.session.user_id);
   if (!urlDatabase[req.params.shortURL] || !userlink[req.params.shortURL]){  //Check if link exists or belong to user
     res.status('400');
     res.send('This url does not exist or not available for you to edit')
@@ -134,36 +141,54 @@ app.post('/urls/:shortURL', (req, res) => {
 });
 
 app.post('/urls/:shortURL/delete', (req, res) => {
-  const userlink = urlsForUser(req.cookies.user_id);
+  const userlink = urlsForUser(req.session.user_id);
   if (!urlDatabase[req.params.shortURL] || !userlink[req.params.shortURL]){  //Check if link exists or belong to user
     res.status('400');
     res.send('This url does not exist or not available for you to delete')
   }
-
   delete urlDatabase[req.params.shortURL];
   res.redirect('/urls');
 });
 
 app.get('/login', (req, res) =>{
-  if (req.cookies.user_id){
+  if (req.session.user_id){
     res.redirect('/urls');
   } else {
     const templateVars = {
-      user: users[req.cookies.user_id]
+      user: users[req.session.user_id]
     };
     res.render('urls_login', templateVars);
   }
 });
 
 app.get ('/register', (req,res) => {
-  const templateVars = {
-    user: users[req.cookies.user_id]
-  };
+  if (req.session.user_id){
+    res.redirect('/urls');
+  } else {
+    const templateVars = {
+    user: users[req.session.user_id]
+    };
   res.render('urls_register', templateVars);
+  }
+});
+
+app.post('/login', (req, res) => {
+  const userAccount = emailLookUp(req.body.email);
+  if (userAccount){ 
+    if (bcrypt.compareSync(req.body.password, userAccount.password)){
+      req.session.user_id = userAccount.id; //issued cookie after log in
+      res.redirect('/urls');
+    } else {
+      res.status('403')
+      res.send('Error 403 - Forbidden!');      
+    }
+  } else {
+    res.status('403')
+    res.send('Error 403 - Forbidden!');
+  }   
 });
 
 app.post ('/register', (req,res) =>{
-  
   if (!req.body.email || !req.body.password || emailLookUp(req.body.email)){
     res.status('400');
     res.send('Error 400 - Bad Request!');
@@ -175,29 +200,12 @@ app.post ('/register', (req,res) =>{
     email: req.body.email,
     password: bcrypt.hashSync(req.body.password, 12)
   };
-  res.cookie('user_id', newID); //issued cookie upon registration
+  req.session.user_id = newID; //issued cookie upon registration
   res.redirect('/urls');
 });
 
-app.post('/login', (req, res) => {
-  const userAccount = emailLookUp(req.body.email);
-  if (userAccount){
-    if (bcrypt.compareSync(req.body.password, userAccount.password)){
-      res.cookie('user_id', userAccount.id); //issued cookie after log in
-      res.redirect('/urls');
-    } else {
-      res.status('403')
-      res.send('Error 403 - Forbidden!');      
-    }
-  } else {
-    res.status('403')
-    res.send('Error 403 - Forbidden!');
-  } 
-  
-});
-
 app.post('/logout', (req, res) => {
-  res.clearCookie('user_id');
+  req.session.user_id = null;
   res.redirect('/urls');
 });
 
